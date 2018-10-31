75ab9de074d7d7fd32dcb9c40a00ee3e10a093ab
KangWei Ng <kangwei9125@gmail.com>
2018-10-31 21:56:59 +0800

    Added some new codes

--- rGroupProj.R
+++ rGroupProj.R
@@ -11,25 +11,66 @@ colNames <- c("Date", "Financial Institution", "Location", "High", "Low", "Open"
 stockDf <- do.call(rbind, lapply(files, function(x) read.csv(x, stringsAsFactors = FALSE,
                   header = TRUE, col.names = colNames)))
 
+companyDetails <- read.csv('../securities.csv', stringsAsFactors = FALSE)
 
-# Exploring the data
+# Check the structure and dimension of the data
 head(stockDf)
-summary(stockDf)
 dim(stockDf)
 str(stockDf)
+summary(stockDf)
+
+head(companyDetails)
+dim(companyDetails)
+str(companyDetails)
 
 # View the data to get a big picture
 View(stockDf)
+View(companyDetails)
 
-# change the date format in the datasets
+# Fix the date format in the dataset
 library(lubridate)
 library(dplyr)
 stockDf$Date <- dmy(stockDf$Date)
+
+companyDetails$Date.first.added <- dmy(companyDetails$Date.first.added)
+
+# Fix the number incorrectly represented as characters
+library(stringr)
+stockDf$Number.of.employees <- str_replace(stockDf$Number.of.employees, pattern = ',', replacement = '')
+stockDf$Number.of.employees <- as.numeric(stockDf$Number.of.employees)
+
+# Check the structure of the dataset after conversion
 str(stockDf)
 glimpse(stockDf)
-View(stockDf)
 
-# Subset by year
-data2012 <- subset(stockDf, stockDf$Period.Ending==ymd('2012-12-31'))
-dim(data2012)
-View(data2012)
+str(companyDetails)
+glimpse(companyDetails)
+
+# Check unique stock
+unique(stockDf$Financial.Institution)
+length(unique(stockDf$Financial.Institution))
+
+unique(companyDetails$Ticker.symbol)
+length(unique(companyDetails$Ticker.symbol))
+
+
+# Function  to get stock details
+getCompanyDetails <- function(tickerSym) {
+  stock <- companyDetails[which(companyDetails$Ticker.symbol==tickerSym),]
+  return(stock)
+}
+
+getCompanyDetails(tickerSym = 'AXP')
+getCompanyDetails(tickerSym = 'BAC')
+getCompanyDetails('C')
+
+# Create a new column for company name
+# This method, merge the 2 datasets, need to remove alot of columns
+mergeData <- merge(stockDf, companyDetails, by.x = c('Financial.Institution'), by.y = c('Ticker.symbol'))
+View(mergeData)
+
+
+stockDf$Company <- lapply(stockDf$Financial.Institution, getCompanyDetails(tickerSym = stockDf$Financial.Institution))
+
+getCompanyDetails(tickerSym = 'AXP')$Security
+mutate(stockDf, getCompanyDetails(stockDf$Financial.Institution)$Security)
