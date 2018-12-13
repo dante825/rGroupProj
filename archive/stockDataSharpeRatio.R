f3aa80eb4d57fc1d2ba22e7f012d24878b52d425
Dante <kangwei9125@gmail.com>
2018-12-13 12:01:09 +0800

    Clean up the repository

---
+++ archive/stockDataSharpeRatio.R
@@ -0,0 +1,183 @@
+# R group project
+
+# Reading the 20 source files
+setwd('./nyse-financial-stocks/')
+files <- list.files(pattern = "*.csv")
+stockCol <- c("Date", "Financial Institution", "Location", "High", "Low", "Open", "Close", 
+              "Aggregated data 2 days", "Aggregated data 3 days", "Aggregated data 5 days",
+              "Volume", "Number of employees", "Net change 0-numeric", "Net change 0-nominal",
+              "Net change 5-numeric", "Net change 5-nominal", "Net change 25-numeric", 
+              "Net change 25-nominal")
+stockDf <- do.call(rbind, lapply(files, function(x) read.csv(x, stringsAsFactors = FALSE,
+                  header = TRUE, col.names = stockCol)))
+
+companyDetails <- read.csv('../securities.csv', stringsAsFactors = FALSE)
+
+# Check the structure and dimension of the data
+head(stockDf)
+dim(stockDf)
+str(stockDf)
+summary(stockDf)
+
+head(companyDetails)
+dim(companyDetails)
+str(companyDetails)
+
+# View the data to get a big picture
+View(stockDf)
+View(companyDetails)
+
+# Fix the date format in the dataset
+library(lubridate)
+library(dplyr)
+stockDf$Date <- dmy(stockDf$Date)
+
+companyDetails$Date.first.added <- dmy(companyDetails$Date.first.added)
+
+# Fix the number incorrectly represented as characters
+library(stringr)
+stockDf$Number.of.employees <- str_replace(stockDf$Number.of.employees, pattern = ',', replacement = '')
+stockDf$Number.of.employees <- as.numeric(stockDf$Number.of.employees)
+
+# Fix the string that should be formatted as factors
+stockDf$Net.change.0.nominal <- as.factor(stockDf$Net.change.0.nominal)
+stockDf$Net.change.5.nominal <- as.factor(stockDf$Net.change.5.nominal)
+stockDf$Net.change.25.nominal <- as.factor(stockDf$Net.change.25.nominal)
+
+companyDetails$SEC.filings <- as.factor(companyDetails$SEC.filings)
+companyDetails$GICS.Sector <- as.factor(companyDetails$GICS.Sector)
+companyDetails$GICS.Sub.Industry <- as.factor(companyDetails$GICS.Sub.Industry)
+
+# Check the structure of the dataset after conversion
+str(stockDf)
+glimpse(stockDf)
+
+str(companyDetails)
+glimpse(companyDetails)
+
+# Check if there is NA in the dataset
+sum(is.na(stockDf))
+sum(is.na(companyDetails))
+
+# Check unique stock
+unique(stockDf$Financial.Institution)
+length(unique(stockDf$Financial.Institution))
+
+unique(companyDetails$Ticker.symbol)
+length(unique(companyDetails$Ticker.symbol))
+
+
+# Function  to get stock details
+getCompanyDetails <- function(tickerSym) {
+  stock <- companyDetails[which(companyDetails$Ticker.symbol==tickerSym),]
+  return(stock)
+}
+
+getCompanyName <- function(tickerSym) {
+  companyName <- companyDetails[which(companyDetails$Ticker.symbol==tickerSym),]$Security
+  return(companyName)
+}
+
+getCompanyDetails(tickerSym = 'AXP')
+getCompanyDetails(tickerSym = 'BAC')
+getCompanyDetails('C')
+
+getCompanyName(tickerSym = 'AXP')
+getCompanyName(tickerSym = 'ABT')
+getCompanyName(tickerSym = 'ALB')
+
+# Merge the 2 dataset
+stockData <- merge(stockDf, companyDetails, by.x = c('Financial.Institution'), by.y = c('Ticker.symbol'))
+View(stockData)
+colnames(stockData)
+
+# Remove unneeded columns
+stockData <- select(stockData, c("Date", "Financial.Institution", "High", "Low", "Open", "Close", 
+                                  "Volume", "Security", "GICS.Sector", "GICS.Sub.Industry"))
+colnames(stockData)
+names(stockData) <- c("Date", "StockCode", "High", "Low", "Open", "Close", "Volume", "Company", "Sector", "Sub-industry")
+colnames(stockData)
+View(stockData)
+
+# Check the number of NAs
+sum(is.na(stockData)) # No NA after removing unnecessary columns
+
+# Visualize some of the data
+plotClosePrice <- function(code) {
+  subStock <- stockData %>% filter(StockCode==code, Date >= dmy('01-01-2017'))
+  plot(x = subStock$Date, y = subStock$Close, main=paste("Closing price for", getCompanyName(code)),
+       xlab="Year 2017", ylab="Closing Price, $", type='l')
+}
+
+plotClosePrice(code="BAC")
+
+# Converting dataframe to timeseries and get the monthly returns
+library(xts)
+library(quantmod)
+
+getMonthlyReturn <- function(code) {
+  df <- stockData %>% filter(StockCode==code, Date >= dmy('01-01-2017')) %>% 
+    select(Date, High, Low, Open, Close, Volume)
+  ts <- xts(df[,-1], order.by = df$Date) 
+  monthly <- monthlyReturn(ts, type='log')
+  return(monthly)
+}
+
+jpmMonthly <- getMonthlyReturn('JPM')
+head(jpmMonthly)
+
+plotMonthlyReturns <- function(monthly, code) {
+  plot(monthly, main=paste("Monthly Returns for", getCompanyName(code)), ylab = "Monthly returns, $", type='l')
+}
+plotMonthlyReturns(jpmMonthly, 'JPM')
+
+
+# Calculate Sharpe ratio 
+library(PerformanceAnalytics)
+sharpeRatio <- round(SharpeRatio(jpmMonthly, Rf=.0003), 4)
+sharpeRatio
+
+# Select a portfolio of stocks: Citigroup Inc (C), Goldman Sachs Group (GS), JPMorgan Chase & Co. (JPM)
+# Sun Trust Bank (STI), Wells Fargo (WFC)
+cMonthly <- getMonthlyReturn('C')
+names(cMonthly) <- 'Citigroup'
+gsMonthly <- getMonthlyReturn('GS')
+names(gsMonthly) <- 'GS'
+jpmMonthly <- getMonthlyReturn('JPM')
+names(jpmMonthly) <- 'JPM'
+stiMonthly <- getMonthlyReturn('STI')
+names(stiMonthly) <- 'STI'
+wfcMonthly <- getMonthlyReturn('WFC')
+names(wfcMonthly) <- 'WFC'
+
+mergedReturns <- merge.xts(cMonthly, gsMonthly, jpmMonthly, stiMonthly, wfcMonthly)
+
+library(dygraphs)
+dygraph(mergedReturns, paste(getCompanyName('C'), 'v', getCompanyName('GS'), 'v', getCompanyName('JPM'),
+                             'v', getCompanyName('STI'), 'v', getCompanyName('WFC'))) %>% 
+  dyAxis('y', label = '%') %>% 
+  dyOptions(colors = RColorBrewer::brewer.pal(3, 'Set2')) %>%
+  dyLegend(show='always')
+
+
+# Allocate weights to the portfolio
+weight <- c(.30, .20, .20, .10, .20)
+
+# Calculate the monthly returns of the portfolio according to the weights
+portfolioReturns <- Return.portfolio(mergedReturns, weights = weight)
+
+dygraph(portfolioReturns, main='Portfolio Monthly Returns') %>%
+  dyAxis('y', label = '%')
+
+# Instead of looking at monthly returns let's look at how $1 would have grown in this portfolio
+dollarGrowth <- Return.portfolio(mergedReturns, weights = weight, wealth.index = TRUE)
+
+dygraph(dollarGrowth, main = 'Growth of $1 Invested in Portfolio') %>% dyAxis('y', label = '%')
+
+# Calculate the Sharpe ratio
+portfolioExcessReturns <- Return.excess(portfolioReturns, Rf=.0003)
+sharpeRatio <- round(mean(portfolioExcessReturns)/StdDev(portfolioExcessReturns), 4)
+sharpeRatio
+
+sharpeRatio <- round(SharpeRatio(portfolioReturns, Rf=.0003), 4)
+sharpeRatio
--- stockDataAnalysis.R
+++ stockDataAnalysis.R
@@ -1,17 +1,22 @@
 # R group project
 
-# Reading the 20 source files
+# Reading the 20 source files at once
 setwd('./nyse-financial-stocks/')
 files <- list.files(pattern = "*.csv")
-stockCol <- c("Date", "Financial Institution", "Location", "High", "Low", "Open", "Close", 
+# The column names for the imported stock datasets
+stockCol <- c("Date", "Stock", "Location", "High", "Low", "Open", "Close", 
               "Aggregated data 2 days", "Aggregated data 3 days", "Aggregated data 5 days",
               "Volume", "Number of employees", "Net change 0-numeric", "Net change 0-nominal",
               "Net change 5-numeric", "Net change 5-nominal", "Net change 25-numeric", 
               "Net change 25-nominal")
+# Read the 20 source files and merge them into a single dataframe
 stockDf <- do.call(rbind, lapply(files, function(x) read.csv(x, stringsAsFactors = FALSE,
-                  header = TRUE, col.names = stockCol)))
+                                                             header = TRUE, col.names = stockCol)))
 
-companyDetails <- read.csv('../securities.csv', stringsAsFactors = FALSE)
+# The column names for the imported company dataset
+companyCol <- c('Stock', 'Company', 'SEC filings', 'GICS Sector', 'GICS Sub Industry', 'HQ Address',
+                'Date first added', 'CIK')
+companyDetails <- read.csv('../securities.csv', stringsAsFactors = FALSE, col.names = companyCol)
 
 # Check the structure and dimension of the data
 head(stockDf)
@@ -48,6 +53,18 @@ companyDetails$SEC.filings <- as.factor(companyDetails$SEC.filings)
 companyDetails$GICS.Sector <- as.factor(companyDetails$GICS.Sector)
 companyDetails$GICS.Sub.Industry <- as.factor(companyDetails$GICS.Sub.Industry)
 
+levels(stockDf$Net.change.0.nominal)
+levels(stockDf$Net.change.5.nominal)
+levels(stockDf$Net.change.25.nominal)
+
+levels(stockDf$Net.change.0.nominal) <- c("Unknown", "Equal", "Negative", "Positive")
+levels(stockDf$Net.change.5.nominal) <- c("Unknown", "Equal", "Negative", "Positive")
+levels(stockDf$Net.change.25.nominal) <- c("Unknown", "Equal", "Negative", "Positive")
+
+levels(companyDetails$SEC.filings)
+levels(companyDetails$GICS.Sector)
+levels(companyDetails$GICS.Sub.Industry)
+
 # Check the structure of the dataset after conversion
 str(stockDf)
 glimpse(stockDf)
@@ -59,22 +76,50 @@ glimpse(companyDetails)
 sum(is.na(stockDf))
 sum(is.na(companyDetails))
 
+# Find out where is the NA
+stockDf[!complete.cases(stockDf),]
+sapply(stockDf, function (x) sum(is.na(x)))
+nrow(stockDf[!complete.cases(stockDf),])
+
+# Most of the NAs are from the column Net.change.25.numeric
+sum(is.na(stockDf$Net.change.25.numeric))
+# Fill in the NAs with the mean from the column
+stockDf$Net.change.25.numeric[is.na(stockDf$Net.change.25.numeric)] <- mean(stockDf$Net.change.25.numeric, 
+                                                                            na.rm=T)
+# Another column with NAs
+sum(is.na(stockDf$Net.change.5.numeric))
+stockDf$Net.change.5.numeric[is.na(stockDf$Net.change.5.numeric)] <- mean(stockDf$Net.change.5.numeric, 
+                                                                            na.rm=T)
+# Removed all the NAs from the stock dataframe
+sum(is.na(stockDf))
+
+# Find the NAs in the company dataframe
+companyDetails[!complete.cases(companyDetails),]
+sapply(companyDetails, function (x) sum(is.na(x)))
+nrow(companyDetails[!complete.cases(companyDetails),])
+sum(is.na(companyDetails$Date.first.added))
+
+# Replace the NAs in the date column with a date
+companyDetails$Date.first.added[is.na(companyDetails$Date.first.added)] <- dmy('01-01-1970')
+
+# Removed all the NAs in the company data frame
+sum(is.na(companyDetails))
+
 # Check unique stock
-unique(stockDf$Financial.Institution)
-length(unique(stockDf$Financial.Institution))
+unique(stockDf$Stock)
+length(unique(stockDf$Stock))
 
-unique(companyDetails$Ticker.symbol)
-length(unique(companyDetails$Ticker.symbol))
+unique(companyDetails$Stock)
+length(unique(companyDetails$Stock))
 
-
-# Function  to get stock details
+# Function  to get company details with the stock code
 getCompanyDetails <- function(tickerSym) {
-  stock <- companyDetails[which(companyDetails$Ticker.symbol==tickerSym),]
+  stock <- companyDetails[which(companyDetails$Stock==tickerSym),]
   return(stock)
 }
 
 getCompanyName <- function(tickerSym) {
-  companyName <- companyDetails[which(companyDetails$Ticker.symbol==tickerSym),]$Security
+  companyName <- companyDetails[which(companyDetails$Stock==tickerSym),]$Company
   return(companyName)
 }
 
@@ -86,98 +131,248 @@ getCompanyName(tickerSym = 'AXP')
 getCompanyName(tickerSym = 'ABT')
 getCompanyName(tickerSym = 'ALB')
 
-# Merge the 2 dataset
-stockData <- merge(stockDf, companyDetails, by.x = c('Financial.Institution'), by.y = c('Ticker.symbol'))
+# Merge the 2 dataset with left join with the similar column Stock
+stockData <- left_join(stockDf, companyDetails, by = c('Stock'))
+colnames(stockData)
+dim(stockData)
+
+# Select the columns for further analysis
+stockData <- select(stockData, c("Date", "Stock", "High", "Low", "Open", "Close", 
+                                 "Volume", "Company", "GICS.Sector", "GICS.Sub.Industry", 
+                                 "Net.change.0.numeric", "Net.change.0.nominal"))
+colnames(stockData)
+dim(stockData)
+
+# Rename the columns to intuitive names
+names(stockData) <- c("Date", "StockCode", "High", "Low", "Open", "Close", "Volume", "Company", 
+                      "Sector", "SubIndustry", "NetChange", "NetChangeNominal")
+colnames(stockData)
 View(stockData)
-colnames(stockData)
 
-# Remove unneeded columns
-stockData <- select(stockData, c("Date", "Financial.Institution", "High", "Low", "Open", "Close", 
-                                  "Volume", "Security", "GICS.Sector", "GICS.Sub.Industry"))
+# Create new columns from the existing columns
+stockData <- stockData %>% mutate(HighLowDiff = High - Low)
 colnames(stockData)
-names(stockData) <- c("Date", "StockCode", "High", "Low", "Open", "Close", "Volume", "Company", "Sector", "Sub-industry")
-colnames(stockData)
-View(stockData)
+glimpse(stockData)
 
-# Check the number of NAs
-sum(is.na(stockData)) # No NA after removing unnecessary columns
+# Cleansing END. The data is cleaned and merged
 
-# Visualize some of the data
-plotClosePrice <- function(code) {
-  subStock <- stockData %>% filter(StockCode==code, Date >= dmy('01-01-2017'))
-  plot(x = subStock$Date, y = subStock$Close, main=paste("Closing price for", getCompanyName(code)),
-       xlab="Year 2017", ylab="Closing Price, $", type='l')
+############# Exploratory Data Analysis #################
+library(ggplot2)
+library(gridExtra)
+
+# Function to get year range for the plot functions
+getYear <- function(year) {
+  vec <- c()
+  if (year == '2013') {
+    vec <- c('2013', '01-01-2013', '31-12-2013')
+  } else if (year == '2014') {
+    vec <- c('2014', '01-01-2014', '31-12-2014')
+  } else if (year == '2015') {
+    vec <- c('2015', '01-01-2015', '31-12-2015')
+  } else if (year == '2016') {
+    vec <- c('2016', '01-01-2016', '31-12-2016')
+  } else if (year == '2017') {
+    vec <- c('2017', '01-01-2017', '31-12-2017')
+  } else {
+    vec <- c()
+  }
+  return (vec)
 }
 
-plotClosePrice(code="BAC")
-
-# Converting dataframe to timeseries and get the monthly returns
-library(xts)
-library(quantmod)
-
-getMonthlyReturn <- function(code) {
-  df <- stockData %>% filter(StockCode==code, Date >= dmy('01-01-2017')) %>% 
-    select(Date, High, Low, Open, Close, Volume)
-  ts <- xts(df[,-1], order.by = df$Date) 
-  monthly <- monthlyReturn(ts, type='log')
-  return(monthly)
+# Closing price of a certain stock
+plotClosePrice <- function(code, year='2017') {
+  yearVec <- getYear(year)
+  subStock <- stockData %>% filter(StockCode==code, Date >= dmy(yearVec[2]) & Date <= dmy(yearVec[3]))
+  ggplot(data = subStock, aes(x=Date, y=Close)) +
+    geom_line(color='Blue') +
+    ggtitle(paste('Closing Price for', getCompanyName(code))) +
+    xlab(yearVec[1]) +
+    ylab('Closing Price, $')
 }
 
-jpmMonthly <- getMonthlyReturn('JPM')
-head(jpmMonthly)
+plotClosePrice(code="BAC", year='2016')
 
-plotMonthlyReturns <- function(monthly, code) {
-  plot(monthly, main=paste("Monthly Returns for", getCompanyName(code)), ylab = "Monthly returns, $", type='l')
+# High Low difference of a stock
+plotHighLowDiff <- function(code, year='2017') {
+  yearVec <- getYear(year)
+  subStock <- stockData %>% filter(StockCode==code, Date >= dmy(yearVec[2]) & Date <= dmy(yearVec[3]))
+  ggplot(data=subStock, aes(x=Date, y=HighLowDiff)) +
+    ggtitle(paste('High-Low Difference for', getCompanyName(code))) +
+    geom_line(color='blue') +
+    xlab(yearVec[1]) +
+    ylab('High-Low Difference, $')
 }
-plotMonthlyReturns(jpmMonthly, 'JPM')
+
+plotHighLowDiff(code='BAC', year='2016')
+
+# Volume
+plotVolume <- function(code, year='2017') {
+  yearVec <- getYear(year)
+  substock <- stockData %>% filter(StockCode==code, Date >= dmy(yearVec[2]) & Date <= dmy(yearVec[3]))
+  ggplot(data=substock, aes(x=Date, y=Volume)) +
+    geom_line(color='blue') +
+    ggtitle(paste('Volume of', getCompanyName(code))) +
+    xlab(yearVec[1]) +
+    ylab('Volume')
+    # scale_y_continuous(labels = scales::comma)
+}
+
+plotVolume(code='BAC', year='2017')
+
+# Comparing the year on year closing price
+BACClo2013 <- plotClosePrice(code = 'BAC', year = '2013')
+BACClo2014 <- plotClosePrice(code = 'BAC', year = '2014')
+BACClo2015 <- plotClosePrice(code = 'BAC', year = '2015')
+BACClo2016 <- plotClosePrice(code = 'BAC', year = '2016')
+BACClo2017 <- plotClosePrice(code = 'BAC', year = '2017')
+grid.arrange(BACClo2013, BACClo2014, BACClo2015, BACClo2016, BACClo2017, ncol=2)
+# Closing price rises at the end of the years
+
+# Comparing the year on year sales volume
+BACVol2013 <- plotVolume(code = 'BAC', year = '2013')
+BACVol2014 <- plotVolume(code = 'BAC', year = '2014')
+BACVol2015 <- plotVolume(code = 'BAC', year = '2015')
+BACVol2016 <- plotVolume(code = 'BAC', year = '2016')
+BACVol2017 <- plotVolume(code = 'BAC', year = '2017')
+grid.arrange(BACVol2013, BACVol2014, BACVol2015, BACVol2016, BACVol2017, ncol=2)
+# Volume drop at the end of the years
+
+# Comparing High low differences
+BACPri2013 <- plotHighLowDiff(code = 'BAC', year = '2013')
+BACPri2014 <- plotHighLowDiff(code = 'BAC', year = '2014')
+BACPri2015 <- plotHighLowDiff(code = 'BAC', year = '2015')
+BACPri2016 <- plotHighLowDiff(code = 'BAC', year = '2016')
+BACPri2017 <- plotHighLowDiff(code = 'BAC', year = '2017')
+grid.arrange(BACPri2013, BACPri2014, BACPri2015, BACPri2016, BACPri2017, ncol=2)
 
 
-# Calculate Sharpe ratio 
-library(PerformanceAnalytics)
-sharpeRatio <- round(SharpeRatio(jpmMonthly, Rf=.0003), 4)
-sharpeRatio
+# Exploring the relationship between High and Volume
+BACStock <- stockData %>% filter(StockCode == 'BAC')
+cor(BACStock$Volume, BACStock$Close)
+cor(BACStock$Volume, BACStock$High)
+ggplot(BACStock, aes(x=High, y=Volume)) +
+  geom_line(color='blue')
 
-# Select a portfolio of stocks: Citigroup Inc (C), Goldman Sachs Group (GS), JPMorgan Chase & Co. (JPM)
-# Sun Trust Bank (STI), Wells Fargo (WFC)
-cMonthly <- getMonthlyReturn('C')
-names(cMonthly) <- 'Citigroup'
-gsMonthly <- getMonthlyReturn('GS')
-names(gsMonthly) <- 'GS'
-jpmMonthly <- getMonthlyReturn('JPM')
-names(jpmMonthly) <- 'JPM'
-stiMonthly <- getMonthlyReturn('STI')
-names(stiMonthly) <- 'STI'
-wfcMonthly <- getMonthlyReturn('WFC')
-names(wfcMonthly) <- 'WFC'
+cor(BACStock$High, BACStock$Close)
+cor(BACStock$Close, BACStock$Low)
+cor(BACStock$High, BACStock$Low)
+cor(BACStock$High, BACStock$Open)
+cor(BACStock$Open, BACStock$Close)
+cor(BACStock$Open, BACStock$High + BACStock$Low)
+ggplot(BACStock, aes(x=Open, y=High)) +
+  geom_point(color='blue') +
+  geom_abline(color='red', size=1)
+ggplot(BACStock, aes(x=Open, y=Low)) +
+  geom_point(color='blue') +
+  geom_abline(color='red', size=1)
+ggplot(BACStock, aes(x=High, y=Low)) +
+  geom_point(color='blue') +
+  geom_abline(color='red', size=1)
+# There is a corelation between High, Close, Low
 
-mergedReturns <- merge.xts(cMonthly, gsMonthly, jpmMonthly, stiMonthly, wfcMonthly)
+Cstock <- stockData %>% filter(StockCode == 'C')
+cor(Cstock$Close, Cstock$High)
+cor(Cstock$Open, Cstock$High)
+cor(Cstock$Open, Cstock$Close)
+cor(Cstock$Open, Cstock$Low)
 
-library(dygraphs)
-dygraph(mergedReturns, paste(getCompanyName('C'), 'v', getCompanyName('GS'), 'v', getCompanyName('JPM'),
-                             'v', getCompanyName('STI'), 'v', getCompanyName('WFC'))) %>% 
-  dyAxis('y', label = '%') %>% 
-  dyOptions(colors = RColorBrewer::brewer.pal(3, 'Set2')) %>%
-  dyLegend(show='always')
+ggplot(Cstock, aes(x = Open, y = High)) +
+  geom_point(color='blue') +
+  geom_abline(color='red', size=1)
+ggplot(Cstock, aes(x = Open, y = Close)) +
+  geom_point(color='blue') +
+  geom_abline(color='red', size=1)
+
+############ Linear Regression ###############
+# Linear Regression with BAC stock
+# Getting the training and testing dataset
+BACStockTrain <- stockData %>% filter(StockCode == 'BAC', Date < dmy('01-01-2017')) %>% 
+  select(StockCode, High, Low, Open, Close)
+BACStockTest <- stockData %>% filter(StockCode == 'BAC', Date >= dmy('01-01-2017')) %>%
+  select(StockCode, High, Low, Open, Close)
+
+# Feature scaling on the dataset
+# trainingSet <- as.data.frame(scale(BACStockTrain[-1]))
+# testSet <- as.data.frame(scale(BACStockTest[-1]))
+
+regressionModel <- lm(formula = High ~ Open, data=BACStockTrain)
+
+y_pred <- predict(regressionModel, newdata = BACStockTest)
+
+# Plot with training set
+ggplot() +
+  geom_point(aes(x = BACStockTrain$Open, y = BACStockTrain$High), color='blue') +
+  geom_line(aes(x = BACStockTrain$Open, y=predict(regressionModel, newdata = BACStockTrain)), color='red',size=1) +
+  labs(title = 'Open vs High (training set)', x = 'Open', y='High')
+
+# Plot with test set
+ggplot() +
+  geom_point(aes(x = BACStockTest$Open, y = BACStockTest$High), colour = 'blue') +
+  geom_line(aes(x = BACStockTest$Open, y = y_pred), colour = 'red', size=1) +
+  labs(title = 'Prediction of High price using Open price (test set)', x='Open', y='High')
 
 
-# Allocate weights to the portfolio
-weight <- c(.30, .20, .20, .10, .20)
+# Linear regression with C stock
+CstockTrain <- stockData %>% filter(StockCode == 'C', Date < dmy('01-01-2017')) %>%
+  select(StockCode, High, Low, Open, Close)
+CstockTest <- stockData %>% filter(StockCode == 'C', Date >= dmy('01-01-2017')) %>%
+  select(StockCode, High, Low, Open, Close)
 
-# Calculate the monthly returns of the portfolio according to the weights
-portfolioReturns <- Return.portfolio(mergedReturns, weights = weight)
+regressionModel <- lm(formula = High ~ Open, data=CstockTrain)
 
-dygraph(portfolioReturns, main='Portfolio Monthly Returns') %>%
-  dyAxis('y', label = '%')
+y_pred <- predict(regressionModel, newdata = CstockTest)
 
-# Instead of looking at monthly returns let's look at how $1 would have grown in this portfolio
-dollarGrowth <- Return.portfolio(mergedReturns, weights = weight, wealth.index = TRUE)
+ggplot() +
+  geom_point(aes(x = CstockTest$Open, y=CstockTest$High), color='blue') +
+  geom_line(aes(x = CstockTest$Open, y=y_pred), color='red', size=1) +
+  labs(title='Prediction of High price with Open price', x='Open', y='High')
 
-dygraph(dollarGrowth, main = 'Growth of $1 Invested in Portfolio') %>% dyAxis('y', label = '%')
+####### ARIMA Implementation ####
+library(MASS)
+library(forecast)
+library(tseries)
 
-# Calculate the Sharpe ratio
-portfolioExcessReturns <- Return.excess(portfolioReturns, Rf=.0003)
-sharpeRatio <- round(mean(portfolioExcessReturns)/StdDev(portfolioExcessReturns), 4)
-sharpeRatio
+full = stockData %>% filter(StockCode == "BAC") %>% dplyr::select(Date,High)
 
-sharpeRatio <- round(SharpeRatio(portfolioReturns, Rf=.0003), 4)
-sharpeRatio
+# predicting only for 2 days ahead 
+train = full[1:1257,] # 1:1000
+test = full[1258:1259,] # 1001:1259
+
+# Autocorrelation And Cross-Correlation Function Estimation
+acf(train$High,lag.max = 20)
+
+# Partial Autocorrelation And Cross-Correlation Function Estimation
+# test indicates data is stationary, time series data is predictable to some degree
+pacf(train$High,lag.max = 20)
+
+diffstock = diff(train$High,1)
+adf.test(train$High)
+
+# Augmented Dickey-Fuller Test to test if model is stationary
+adf.test(diffstock) 
+
+pricearima = ts(train$High, start = c(2012,11,30), frequency = 365)
+
+# auto arima automatically tunes the model
+fitStock = auto.arima(pricearima) 
+
+fitStock
+ggplot2::autoplot(pricearima,main="High Price Values for BAC")
+
+forecastedValues = forecast(fitStock, h=2) #259
+forecastedValues
+
+# higher level view of the time series + forecast
+ggplot2::autoplot(forecast(fitStock, h=2), include = 50)
+# closer look at the time series + forecast
+ggplot2::autoplot(forecast(fitStock, h=2), include = 10) 
+
+# evaluating the mean percentage error of the forecasted prices
+finalForecastedValues = as.numeric(forecastedValues$mean)
+
+compare = data.frame(test$High,finalForecastedValues)
+columns = c("ActualPrice","ForecastedPrice")
+names(compare) = columns
+percentage_error = ((compare$ActualPrice - compare$ForecastedPrice)/compare$ActualPrice)
+percentage_error
+mean(percentage_error) 
